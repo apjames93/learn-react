2b2b40c8535231d6538535469cf8b4d5a401a4a4
Alex James <apjames93@gmail.com>
2017-10-21 15:12:02 -0600

    [onClick]
    set up a few events handlers messing around

--- public/src/app.js
+++ public/src/app.js
@@ -1,7 +1,44 @@
-console.log('its alive')
+console.log('its alive');
 
-// JSX - JavaScript XML
-var template = <h1>Indecision App</h1>;
-var appRoot = document.getElementById('app');
+const app = {
+    title: 'Indecision App',
+    subtitle: 'Put your life in the hands of a computer',
+    options: ['One', 'Two']
+  };
+  const template = (
+    <div>
+      <h1>{app.title}</h1>
+      {app.subtitle && <p>{app.subtitle}</p>}
+      <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
+      <ol>
+        <li>Item one</li>
+        <li>Item two</li>
+      </ol>
+    </div>
+  );
+  
+  let count = 0;
+  const addOne = () => {
+    console.log('addOne');
+  };
 
-ReactDOM.render(template, appRoot)
+  const minusOne = () => {
+    console.log('minusOne');
+  };
+
+  const reset = () => {
+    console.log('reset');
+  };
+  
+  const templateTwo = (
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={addOne}>+1</button>
+      <button onClick={minusOne}>-1</button>
+      <button onClick={reset}>reset</button>
+    </div>
+  );
+  
+  const appRoot = document.getElementById('app');
+  
+  ReactDOM.render(templateTwo, appRoot);
