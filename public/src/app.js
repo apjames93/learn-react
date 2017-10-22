da1d2ea179bdc4ab9a3073fcce812dd8961c7adf
Alex James <apjames93@gmail.com>
2017-10-21 22:04:46 -0600

    [app]
    small app to learn some react add items to a list and pick a random option from the list you make

--- public/src/app.js
+++ public/src/app.js
@@ -1,44 +1,57 @@
-console.log('its alive');
+console.log('App.js is running!');
 
 const app = {
-    title: 'Indecision App',
-    subtitle: 'Put your life in the hands of a computer',
-    options: ['One', 'Two']
-  };
+  title: 'Indecision App',
+  subtitle: 'Put your life in the hands of a computer',
+  options: []
+};
+
+const onFormSubmit = (e) => {
+  e.preventDefault();
+
+  const option = e.target.elements.option.value;
+
+  if (option) {
+    app.options.push(option);
+    e.target.elements.option.value = '';
+    render();
+  }
+};
+
+const onRemoveAll = () => {
+  app.options = [];
+  render();
+};
+
+const onMakeDecision = () => {
+  const randomNum = Math.floor(Math.random() * app.options.length);
+  const option = app.options[randomNum];
+  alert(option);
+};
+
+const appRoot = document.getElementById('app');
+
+const render = () => {
   const template = (
     <div>
       <h1>{app.title}</h1>
       {app.subtitle && <p>{app.subtitle}</p>}
       <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
+      <button disabled={app.options.length === 0} onClick={onMakeDecision}>What should I do?</button>
+      <button onClick={onRemoveAll}>Remove All</button>
       <ol>
-        <li>Item one</li>
-        <li>Item two</li>
+        {
+          app.options.map((option) => <li key={option}>{option}</li>)
+        }
       </ol>
+      <form onSubmit={onFormSubmit}>
+        <input type="text" name="option" />
+        <button>Add Option</button>
+      </form>
     </div>
   );
-  
-  let count = 0;
-  const addOne = () => {
-    console.log('addOne');
-  };
 
-  const minusOne = () => {
-    console.log('minusOne');
-  };
+  ReactDOM.render(template, appRoot);
+};
 
-  const reset = () => {
-    console.log('reset');
-  };
-  
-  const templateTwo = (
-    <div>
-      <h1>Count: {count}</h1>
-      <button onClick={addOne}>+1</button>
-      <button onClick={minusOne}>-1</button>
-      <button onClick={reset}>reset</button>
-    </div>
-  );
-  
-  const appRoot = document.getElementById('app');
-  
-  ReactDOM.render(templateTwo, appRoot);
+render();
