5ece50142b76943a2a13592f1763d5373bf1fbe9
Alex James <apjames93@gmail.com>
2017-10-21 23:59:08 -0600

    [Componet]
    setting up componets and passing props to them

--- public/src/app.js
+++ public/src/app.js
@@ -1,13 +1,72 @@
-class  Header extends React.Component {
+class IndecisionApp extends React.Component {
   render() {
-    return <p>this is the header</p>
+    const title = 'Indecision';
+    const subTitle = 'Put your life in the hands of a computer';
+    const options = ['this one', 'thing two', 'this three', 'thing four']
+
+    return (
+      <div>
+        <Header title={title}/>
+        <Action/>
+        <Options options={options}/>
+        <AddOptions/>
+      </div>
+    )
   }
 }
 
-const jsx = (
-  <div>
-    <h1>Title</h1>
-    <Header/>
-  </div>
-)
-ReactDOM.render(jsx, document.getElementById('app'))
+class  Header extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>{this.props.title}</h1>
+        <h2>{this.props.subTitle}</h2>
+      </div>
+    )
+  }
+}
+
+class Action extends React.Component {
+  render() {
+    return (
+      <div>
+        <button>What Should I do?</button>
+      </div>
+    )
+  }
+}
+
+class Options extends React.Component {
+  render() {
+    return (
+      <div>
+        {
+          this.props.options.map((option) => <Option key={option} optionText={option}/> )
+        }
+      </div>
+    )
+  }
+}
+
+class Option extends React.Component { 
+  render() {
+    return ( 
+      <div>
+        {this.props.optionText}
+      </div>
+    )
+  }
+}
+
+class AddOptions extends React.Component {
+  render() {
+    return (
+      <div>
+        add option componet here
+      </div>
+    )
+  }
+}
+
+
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'))
