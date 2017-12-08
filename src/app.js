31abcbbd6beee40131fac4226f3d7c937085448e
Alex James <apjames93@gmail.com>
2017-12-07 23:40:14 -0700

    [example]
    new syntax using plugin transform-class-properties

--- src/app.js
+++ src/app.js
@@ -3,3 +3,24 @@ import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp'
 
  ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+
+
+
+ 
+ class OldSyntax {
+     constructor() {
+         this.name = 'oldSyntax';
+     }
+ }
+
+ const oldSyntax = new OldSyntax();
+ console.log(oldSyntax)
+
+ //-------------------
+
+ class NewSyntax {
+     name = 'NewSyntax'
+ }
+
+ const newSyntax = new NewSyntax();
+ console.log(newSyntax)
