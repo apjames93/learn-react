e6f326f1239ea9153c1eae85e85f90b461946045
Alex James <apjames93@gmail.com>
2017-12-07 23:31:34 -0700

    [components]
    set up components folder and moved all of my components there

---
+++ src/components/Options.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Option from './Option';
+
+
+const Options = (props) => {
+    return (
+      <div>
+        <button onClick={props.handleDeleteOptions}>Remove All</button>
+        {props.options.length === 0 && <p>add a option to start</p>}
+        
+        {
+          props.options.map((option) => (
+            <Option
+              key={option}
+              optionText={option}
+              handleDeleteOption={props.handleDeleteOption}
+            />
+          ))
+        }
+      </div>
+    );
+  };
+
+  export default Option;
+  
