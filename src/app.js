b9d021189b055d4d15f9310d8eace96532e6d972
Alex James <apjames93@gmail.com>
2017-12-09 08:11:41 -0700

    [example]
    removed example

--- src/app.js
+++ src/app.js
@@ -3,37 +3,3 @@ import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp'
 
  ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
-
-
-
-
- class OldSyntax {
-     constructor() {
-         this.name = 'oldSyntax';
-         this.getGreeting = this.getGreeting.bind(this);
-     }
-     getGreeting() {
-         return 'Hi ${this.name}'
-     }
- }
-
- const oldSyntax = new OldSyntax();
- console.log(oldSyntax);
-
- const getGreeting = oldSyntax.getGreeting;
-
- console.log(getGreeting())
-
- //-------------------
-
- class NewSyntax {
-     name = 'NewSyntax'
-     getGreeting = () => {
-        return 'Hi ${this.name}'        
-     }
- }
-
- const newSyntax = new NewSyntax();
- console.log(newSyntax)
- const newGetGreeting = newSyntax.getGreeting;
- console.log(newGetGreeting())
